ac26721988c78557ef97a4e70ae415b3f4cb59ed
Kai Shiao <kshiao13@gmail.com>
2016-05-09 15:32:49 -0700

    Programmed button for FB page.

--- jquery/home.js
+++ jquery/home.js
@@ -2,5 +2,15 @@ function homePage(){
 	location.href='home.html';
 }
 
+function facebookPage(){
+	location.href='https://www.facebook.com/foodstop.landing';
+}
+
+function twitterPage(){
+	location.href='';
+}
+
 $('.navbar-header').find('button').eq(0).on('click',homePage);
+$('.navbar-header').find('button').eq(1).on('click',facebookPage);
+$('.navbar-header').find('button').eq(2).on('click',twitterPage);
 
