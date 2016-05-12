c03aafbbcc4b49088cabc0ee5dda1d255fe065b6
Kai Shiao <kshiao13@gmail.com>
2016-05-12 11:04:29 -0700

    Programmed buttons.

--- jquery/home.js
+++ jquery/home.js
@@ -6,11 +6,5 @@ function facebookPage(){
 	location.href='https://www.facebook.com/foodstop.landing';
 }
 
-function twitterPage(){
-	location.href='';
-}
-
 $('.navbar-header').find('button').eq(0).on('click',homePage);
 $('.navbar-header').find('button').eq(1).on('click',facebookPage);
-$('.navbar-header').find('button').eq(2).on('click',twitterPage);
-
